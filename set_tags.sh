e8ef53e51099e3480d764611b937793f2facb5d8
Yasser Hossam <yhossam@inmobly.com>
2018-10-25 13:28:05 +0200

    Update tags script

--- set_tags.sh
+++ set_tags.sh
@@ -13,7 +13,7 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
       echo -e "Starting to tag commit.\n"
 
       git config --global user.email "yhossam@inmobly.com"
-      git config --global user.name "Yasser Hossam"
+      git config --global user.name "YHossam"
 
       # Add tag and push to master.
       git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
